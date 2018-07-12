dee7693fb3571e11dc20c7e2eaac9bdb88efa510
Uzonyi Ákos <uzonyi.akos@gmail.com>
2018-07-12 10:54:12 +0200

    Add BKS-V1 version keystore for old devices

--- android/app/src/main/java/com/tisza/tarock/gui/MainActivity.java
+++ android/app/src/main/java/com/tisza/tarock/gui/MainActivity.java
@@ -268,8 +268,10 @@ public class MainActivity extends Activity implements MessageHandler, GameListAd
 
 			try
 			{
+				String trustStoreFile = Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1 ? "truststore.bks" : "truststore.v1.bks";
+
 				KeyStore ks = KeyStore.getInstance("BKS");
-				ks.load(getAssets().open("truststore"), "000000".toCharArray());
+				ks.load(getAssets().open(trustStoreFile), "000000".toCharArray());
 
 				KeyManagerFactory kmf = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
 				kmf.init(ks, "000000".toCharArray());

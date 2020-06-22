e473533fe3306ebab993d4c6dc1a48b0e7e4e9c7
Uzonyi Ákos <uzonyi.akos@gmail.com>
2020-06-22 18:04:41 +0200

    Release 3.4

---
+++ android/app/src/main/java/com/tisza/tarock/gui/DoubleClickListener.java
@@ -0,0 +1,37 @@
+package com.tisza.tarock.gui;
+
+import android.content.*;
+import android.os.*;
+import android.view.*;
+import androidx.preference.*;
+
+public class DoubleClickListener implements View.OnClickListener
+{
+	private static final int doubleClickDelay = 400;
+
+	private final boolean doubleClick;
+	private final View.OnClickListener clickListener;
+
+	private long lastClickTime = 0;
+
+	protected DoubleClickListener(Context context, View.OnClickListener clickListener)
+	{
+		this.clickListener = clickListener;
+		doubleClick = PreferenceManager.getDefaultSharedPreferences(context).getBoolean("double_click", false);
+	}
+
+	@Override
+	public final void onClick(View view)
+	{
+		if (!doubleClick)
+		{
+			clickListener.onClick(view);
+			return;
+		}
+
+		long time = SystemClock.elapsedRealtime();
+		if (time < lastClickTime + doubleClickDelay)
+			clickListener.onClick(view);
+		lastClickTime = time;
+	}
+}

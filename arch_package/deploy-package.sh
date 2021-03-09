6d7402474ea461794a62651417a47c8fb455b9c7
Ákos Uzonyi <uzonyi.akos@gmail.com>
2021-03-09 17:33:39 +0100

    deploy-package.sh: xz -> zst

--- arch_package/deploy-package.sh
+++ arch_package/deploy-package.sh
@@ -9,8 +9,8 @@ PORT=3125
 PKG_NAME=tarock-server
 VERSION=$(git describe --tags --dirty | tr - _)
 
-PKG_FILE="$PROJECT_DIR/arch_package/package/$PKG_NAME-$VERSION-1-any.pkg.tar.xz"
-TMP_FILE="/tmp/$PKG_NAME.tar.xz"
+PKG_FILE="$PROJECT_DIR/arch_package/package/$PKG_NAME-$VERSION-1-any.pkg.tar.zst"
+TMP_FILE="/tmp/$PKG_NAME.tar.zst"
 
 scp -P "$PORT" "$PKG_FILE" "$USER@$HOST:$TMP_FILE"
 ssh "$USER@$HOST" -p "$PORT" << EOF

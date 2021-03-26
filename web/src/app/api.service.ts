cecf8abca2f142c68cb3ee083c5779778fe310e4
Ákos Uzonyi <uzonyi.akos@gmail.com>
2021-03-26 19:22:51 +0100

    api.service: add more api endpoints

--- web/src/app/api.service.ts
+++ web/src/app/api.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { GameSession } from './game-objects';
+import { Action, Chat, DoubleRoundType, Game, GameSession, GameState, GameType } from './game-objects';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -14,5 +15,67 @@ export class ApiService {
 
   getGameSessions(): Observable<GameSession[]> {
     return this.http.get<GameSession[]>(`${this.baseUrl}/gameSessions`);
+  }
+
+  getGameSession(id: number): Observable<GameSession> {
+    return this.http.get<GameSession>(`${this.baseUrl}/gameSessions/${id}`);
+  }
+
+  createGameSession(type: GameType, doubleRoundType: DoubleRoundType): Observable<number> {
+    let body = {'type': type, 'doubleRoundType': doubleRoundType};
+    return this.http.post<Response>(`${this.baseUrl}/gameSessions`, body).pipe(
+      map((response) => {
+        let arr = response.headers.get('Location')!.split('/');
+        return Number(arr[arr.length - 1]);
+      })
+    );
+  }
+
+  deleteGameSession(id: number): Observable<{}> {
+    return this.http.delete(`${this.baseUrl}/gameSessions/${id}`);
+  }
+
+  joinGameSession(id: number): Observable<{}> {
+    return this.http.post(`${this.baseUrl}/gameSessions/${id}/join`, null);
+  }
+
+  leaveGameSession(id: number): Observable<{}> {
+    return this.http.post(`${this.baseUrl}/gameSessions/${id}/leave`, null);
+  }
+
+  startGameSession(id: number): Observable<{}> {
+    return this.http.post(`${this.baseUrl}/gameSessions/${id}/start`, null);
+  }
+
+  getGame(id: number): Observable<Game> {
+    return this.http.get<Game>(`${this.baseUrl}/games/${id}`);
+  }
+
+  getChat(gameSessionId: number, from?: number): Observable<Chat[]> {
+    let params: any = {}
+    if (from !== undefined)
+      params.from = String(from);
+    return this.http.get<Chat[]>(`${this.baseUrl}/gameSessions/${gameSessionId}/chat`, {params: params});
+  }
+
+  postChat(gameSessionId: number, message: string): Observable<{}> {
+    let body = {'message': message};
+    return this.http.post(`${this.baseUrl}/games/${gameSessionId}/chat`, body);
+  }
+
+  getActions(gameId: number, from?: number): Observable<Action[]> {
+    let params: any = {}
+    if (from !== undefined)
+      params.from = String(from);
+    return this.http.get<Action[]>(`${this.baseUrl}/games/${gameId}/actions`, {params: params});
+  }
+
+  postAction(gameId: number, action: string): Observable<{}> {
+    let body = {'action': action};
+    return this.http.post(`${this.baseUrl}/games/${gameId}/actions`, body);
+  }
+
+  getGameState(id: number): Observable<GameState> {
+    return this.http.get<GameState>(`${this.baseUrl}/games/${id}/state`);
   }
 }
--- web/src/app/game-objects.ts
+++ web/src/app/game-objects.ts
@@ -1,3 +1,9 @@
+export type GameType = 'paskievics' | 'illusztralt' | 'magas' | 'zebi';
+export type DoubleRoundType = 'none' | 'peculating' | 'stacking' | 'multiplying';
+export type GameSessionState = 'lobby' | 'game' | 'deleted';
+export type Phase = 'bidding' | 'folding' | 'calling' | 'announcing' | 'gameplay' | 'end' | 'interrupted';
+export type Team = 'caller' | 'opponent';
+
 export interface User {
   id: number,
   name: string,
@@ -13,10 +19,56 @@ export interface Player {
 
 export interface GameSession {
   id: number;
-  type: "paskievics"|"illusztralt"|"magas"|"zebi";
-  doubleRoundType: "none"|"peculating"|"stacking"|"multiplying";
-  state: "lobby"|"game"|"deleted";
+  type: GameType;
+  doubleRoundType: DoubleRoundType;
+  state: GameSessionState;
   players: [Player];
-  currentGameID: number;
+  currentGameId: number | null;
   createTime: number;
+}
+
+export interface Game {
+  id: number;
+  type: GameType;
+  players: [Player];
+  gameSessionId: number;
+  createTime: number;
+}
+
+export interface Action {
+  ordinal: number;
+  action: string;
+  seat: number;
+  time: number;
+}
+
+export interface Chat {
+  userID: number;
+  message: string;
+  time: number;
+}
+
+export interface GameState {
+  cards: (string[] | null)[];
+  phase: Phase;
+  turn: [boolean];
+  canThrowCards: boolean;
+  teamInfo: [Team | null];
+  availableActions: [string];
+  tarockFoldCount: [number];
+  callerTarockFold: [string];
+  currentTrick: [string | null];
+  previousTrick: [string] | null;
+  previousTrickWinner: number | null;
+  statistics: {
+    callerCardPoints: number;
+    opponentCardPoints: number;
+    announcementResults: [{
+      'announcement': string;
+      'points': number;
+      'team': Team;
+    }],
+    sumPoints: number;
+    pointMultiplier: number;
+  }
 }

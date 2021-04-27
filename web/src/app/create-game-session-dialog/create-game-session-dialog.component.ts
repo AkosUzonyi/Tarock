fffac4fcc475f450c4072f7388dfdb944e6d6082
Ákos Uzonyi <uzonyi.akos@gmail.com>
2021-04-27 22:51:50 +0200

    Store game type / double round type in localStorage

--- web/src/app/create-game-session-dialog/create-game-session-dialog.component.ts
+++ web/src/app/create-game-session-dialog/create-game-session-dialog.component.ts
@@ -15,16 +15,17 @@ export class CreateGameSessionDialogComponent implements OnInit {
     private apiService: ApiService
   ) { }
 
-  gameType: GameType = 'paskievics';
-  doubleRoundType: DoubleRoundType = 'none';
+  gameType: GameType = localStorage.getItem('defaultGameType') as GameType ?? 'paskievics';
+  doubleRoundType: DoubleRoundType = localStorage.getItem('defaultDoubleRoundType') as DoubleRoundType ?? 'none';
 
   ngOnInit(): void {
     this.dialogRef.disableClose = false;
   }
 
   create() {
+    localStorage.setItem('defaultGameType', this.gameType);
+    localStorage.setItem('defaultDoubleRoundType', this.doubleRoundType);
     this.apiService.createGameSession(this.gameType, this.doubleRoundType).subscribe();
     this.dialogRef.close();
   }
-
 }

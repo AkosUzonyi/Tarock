AkosUzonyi/Tarock

commit bca7292573830f6d4049d71dd068f406d46c3cac

Hkp: must be announced if any card is called (not just the XX).

server/src/main/java/com/tisza/tarock/game/phase/Game.java
@@ -28,9 +28,9 @@ public class Game
 	private PlayerSeat bidWinnerPlayer = null;
 	private int winnerBid;
 
-	private Map<Team, List<Card>> skartForTeams = new HashMap<>();
-	private PlayerSeat playerSkarted20 = null;
+	private PlayerSeatMap<Collection<Card>> skartedCards = new PlayerSeatMap<>();
 
+	private Card calledCard = null;
 	private PlayerPairs playerPairs = null;
 	private boolean isSoloIntentional = false;
 	private Invitation invitAccepted = null;
@@ -52,11 +52,6 @@ public class Game
 		{
 			playersCards.put(player, new PlayerCards());
 			wonCards.put(player, new ArrayList<>());
-		}
-
-		for (Team t : Team.values())
-		{
-			skartForTeams.put(t, new ArrayList<>());
 		}
 	}
 
@@ -209,24 +204,27 @@ public class Game
 		return winnerBid;
 	}
 
-	void addCardToSkart(Team team, Card card)
+	void setSkart(PlayerSeat player, Collection<Card> cards)
 	{
-		skartForTeams.get(team).add(card);
+		skartedCards.put(player, cards);
 	}
 
-	public List<Card> getSkartForTeam(Team team)
+	public Collection<Card> getSkart(PlayerSeat player)
 	{
-		return skartForTeams.get(team);
+		return skartedCards.get(player);
 	}
 
-	void setPlayerSkarted20(PlayerSeat playerSkarted20)
+	public Card getCalledCard()
 	{
-		this.playerSkarted20 = playerSkarted20;
+		return calledCard;
 	}
 
-	public PlayerSeat getPlayerSkarted20()
+	void setCalledCard(Card card)
 	{
-		return playerSkarted20;
+		calledCard = card;
+
+		if (invitSent != null && card.equals(invitSent.getCard()))
+			invitAccepted = invitSent;
 	}
 
 	void setPlayerPairs(PlayerPairs playerPairs)
@@ -249,24 +247,9 @@ public class Game
 		return isSoloIntentional;
 	}
 
-	void invitAccepted()
-	{
-		invitAccepted = invitSent;
-	}
-
 	public Invitation getInvitAccepted()
 	{
 		return invitAccepted;
-	}
-
-	void setPlayerToAnnounceSolo(PlayerSeat playerToAnnounceSolo)
-	{
-		this.playerToAnnounceSolo = playerToAnnounceSolo;
-	}
-
-	public PlayerSeat getPlayerToAnnounceSolo()
-	{
-		return playerToAnnounceSolo;
 	}
 
 	public AnnouncementsState getAnnouncementsState()
@@ -320,12 +303,14 @@ public class Game
 				points += c.getPoints();
 
 		if (countSelfTeamSkart)
-			for (Card c : getSkartForTeam(team))
-				points += c.getPoints();
+			for (PlayerSeat player : playerPairs.getPlayersInTeam(team))
+				for (Card c : skartedCards.get(player))
+					points += c.getPoints();
 
 		if (countOpponentTeamSkart)
-			for (Card c : getSkartForTeam(team.getOther()))
-				points += c.getPoints();
+			for (PlayerSeat player : playerPairs.getPlayersInTeam(team.getOther()))
+				for (Card c : skartedCards.get(player))
+					points += c.getPoints();
 
 		return points;
 	}

server/src/main/java/com/tisza/tarock/game/TeamInfoTracker.java
@@ -95,7 +95,7 @@ public class TeamInfoTracker implements EventHandler
 		if (game.getInvitAccepted() != null && !inviterSkartedTarock)
 			revealAllTeamInfo();
 
-		if (game.getSkartForTeam(Team.CALLER).contains(card))
+		if (game.getSkart(game.getBidWinnerPlayer()).contains(card))
 			revealAllTeamInfo();
 	}
 

server/src/main/java/com/tisza/tarock/game/announcement/HivatalbolKontraParti.java
@@ -24,12 +24,6 @@ public class HivatalbolKontraParti extends AnnouncementWrapper
 	@Override
 	public boolean canBeAnnounced(IAnnouncing announcing)
 	{
-		if (announcing.isAnnounced(announcing.getCurrentTeam(), Announcements.hkp))
-			return false;
-		
-		if (announcing.getCurrentPlayer() != announcing.getPlayerToAnnounceSolo())
-			return false;
-		
-		return announcing.getContraLevel(Team.CALLER, Announcements.jatek) == 0;
+		return announcing.shouldHkpBeAnnounced();
 	}
 }

server/src/main/java/com/tisza/tarock/game/announcement/IAnnouncing.java
@@ -22,6 +22,6 @@ public interface IAnnouncing
 	PlayerPairs getPlayerPairs();
 	boolean canAnnounce(AnnouncementContra a);
 	PlayerCards getCards(PlayerSeat player);
-	PlayerSeat getPlayerToAnnounceSolo();
+	boolean shouldHkpBeAnnounced();
 	GameType getGameType();
 }

server/src/main/java/com/tisza/tarock/game/phase/Announcing.java
@@ -64,7 +64,7 @@ class Announcing extends Phase implements IAnnouncing
 		if (player != currentPlayer)
 			return false;
 		
-		if (Announcements.hkp.canBeAnnounced(this))
+		if (shouldHkpBeAnnounced())
 		{
 			//game.sendEvent(player, new EventActionFailed(Reason.CONTRAJATEK_REQUIRED));
 			return false;
@@ -130,11 +130,9 @@ class Announcing extends Phase implements IAnnouncing
 			}
 		}
 		
-		if (Announcements.hkp.canBeAnnounced(this))
-		{
+		if (shouldHkpBeAnnounced())
 			list.remove(new AnnouncementContra(Announcements.jatek, 1));
-		}
-		
+
 		game.sendEvent(currentPlayer, Event.availableAnnouncements(list));
 		game.broadcastEvent(Event.turn(currentPlayer));
 	}
@@ -144,10 +142,10 @@ class Announcing extends Phase implements IAnnouncing
 	{
 		Team currentPlayerTeam = getCurrentTeam();
 		Announcement a = ac.getAnnouncement();
-		
-		if (ac.equals(new AnnouncementContra(Announcements.jatek, 1)) && Announcements.hkp.canBeAnnounced(this))
+
+		if (ac.equals(new AnnouncementContra(Announcements.jatek, 1)) && shouldHkpBeAnnounced())
 			return false;
-		
+
 		if (ac.getContraLevel() == 0)
 		{
 			return (!needsIdentification() || !a.requireIdentification()) &&
@@ -250,9 +248,15 @@ class Announcing extends Phase implements IAnnouncing
 	}
 
 	@Override
-	public PlayerSeat getPlayerToAnnounceSolo()
+	public boolean shouldHkpBeAnnounced()
 	{
-		return game.getPlayerToAnnounceSolo();
+		if (isAnnounced(getCurrentTeam(), Announcements.hkp))
+			return false;
+
+		if (getContraLevel(Team.CALLER, Announcements.jatek) > 0)
+			return false;
+
+		return game.getSkart(currentPlayer).contains(game.getCalledCard());
 	}
 
 	@Override

server/src/main/java/com/tisza/tarock/game/phase/Calling.java
@@ -29,13 +29,14 @@ class Calling extends Phase
 		callerPlayer = game.getBidWinnerPlayer();
 		
 		canCallAnyTarock = false;
-		for (Card c : game.getSkartForTeam(Team.OPPONENT))
+		for (PlayerSeat player : PlayerSeat.getAll())
 		{
-			if (c instanceof TarockCard)
-			{
-				canCallAnyTarock = true;
-				break;
-			}
+			if (player == callerPlayer)
+				continue;
+
+			for (Card c : game.getSkart(player))
+				if (c instanceof TarockCard)
+					canCallAnyTarock = true;
 		}
 
 		sendAvailableCalls();
@@ -66,26 +67,14 @@ class Calling extends Phase
 		//if the player called a card that had been skarted
 		if (calledPlayer == null)
 		{
-			if (game.getSkartForTeam(Team.CALLER).contains(card))
+			if (game.getSkart(game.getBidWinnerPlayer()).contains(card))
 				game.setSoloIntentional();
 
 			calledPlayer = callerPlayer;
-			
-			if (card.equals(Card.getTarockCard(20)) && game.getPlayerSkarted20() != callerPlayer)
-			{
-				if (game.getPlayerSkarted20() == null)
-					throw new RuntimeException();
-				game.setPlayerToAnnounceSolo(game.getPlayerSkarted20());
-			}
 		}
 
+		game.setCalledCard(card);
 		game.setPlayerPairs(new PlayerPairs(callerPlayer, calledPlayer));
-
-		Invitation invit = game.getInvitSent();
-		if (invit != null && card.equals(invit.getCard()))
-		{
-			game.invitAccepted();
-		}
 
 		game.broadcastEvent(Event.call(player, card));
 		game.changePhase(new Announcing(game));

server/src/main/java/com/tisza/tarock/game/phase/Changing.java
@@ -95,18 +95,10 @@ class Changing extends Phase
 		
 		int tarockCount = 0;
 		for (Card c : cardsToSkart)
-		{
 			if (c instanceof TarockCard)
-			{
 				tarockCount++;
-				if (c.equals(Card.getTarockCard(20)))
-				{
-					game.setPlayerSkarted20(player);
-				}
-			}
 
-			game.addCardToSkart(player == game.getBidWinnerPlayer() ? Team.CALLER : Team.OPPONENT, c);
-		}
+		game.setSkart(player, cardsToSkart);
 		tarockCounts.put(player, tarockCount);
 		
 		skartingPlayerCards.removeCards(cardsToSkart);
@@ -118,7 +110,7 @@ class Changing extends Phase
 		{
 			game.broadcastEvent(Event.skartTarock(tarockCounts));
 
-			List<Card> callerSkartedTarocks = game.getSkartForTeam(Team.CALLER).stream().filter(c -> c instanceof TarockCard).collect(Collectors.toList());
+			List<Card> callerSkartedTarocks = game.getSkart(game.getBidWinnerPlayer()).stream().filter(c -> c instanceof TarockCard).collect(Collectors.toList());
 			if (!callerSkartedTarocks.isEmpty())
 				game.broadcastEvent(Event.skart(game.getBidWinnerPlayer(), callerSkartedTarocks));
 

commit 7bf3b6cf484f8dcd87e97b5c660da3177c26c422

Fix card point calculation from folded cards
A bug was introduced recently that caused the called player's folded cards added to the caller team points instead of the opponent.

Fixes: bca7292 "Hkp: must be announced if any card is called (not just the XX)."

server/src/main/java/com/tisza/tarock/game/phase/Game.java
@@ -347,33 +347,22 @@ public class Game
 
 	public int calculateGamePoints(Team team)
 	{
-		boolean countSelfTeamSkart = true;
-		boolean countOpponentTeamSkart = false;
-
-		if (playerPairs.isSolo() && !isSoloIntentional)
-		{
-			if (team == Team.CALLER)
-				countOpponentTeamSkart = true;
-
-			if (team == Team.OPPONENT)
-				countSelfTeamSkart = false;
-		}
-
 		int points = 0;
 
 		for (PlayerSeat player : playerPairs.getPlayersInTeam(team))
 			for (Card c : getWonCards(player))
 				points += c.getPoints();
 
-		if (countSelfTeamSkart)
-			for (PlayerSeat player : playerPairs.getPlayersInTeam(team))
-				for (Card c : foldedCards.get(player))
-					points += c.getPoints();
+		boolean isCallerTeam = team == Team.CALLER;
+		boolean callerGetsAllFold = playerPairs.isSolo() && !isSoloIntentional;
+		for (PlayerSeat player : PlayerSeat.getAll())
+		{
+			boolean isCallerPlayer = player == playerPairs.getCaller();
 
-		if (countOpponentTeamSkart)
-			for (PlayerSeat player : playerPairs.getPlayersInTeam(team.getOther()))
+			if (callerGetsAllFold ? isCallerTeam : isCallerPlayer == isCallerTeam)
 				for (Card c : foldedCards.get(player))
 					points += c.getPoints();
+		}
 
 		return points;
 	}
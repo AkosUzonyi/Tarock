6904dc57d422ab50dad5c92021ab964a232fb5b8
Ákos Uzonyi <uzonyi.akos@gmail.com>
2021-05-04 12:51:43 +0200

    ListDeferredResultService: make Request an inner class
    Also if the notifier transaction is rolled back, add the Request back to the list, instead of returning with the bit hacky 408 status code.

--- server/src/main/java/com/tisza/tarock/spring/service/ListDeferredResultService.java
+++ server/src/main/java/com/tisza/tarock/spring/service/ListDeferredResultService.java
@@ -15,12 +15,12 @@ import java.util.function.*;
 @Service
 public class ListDeferredResultService<T>
 {
-	private final Map<Integer, Collection<Request<T>>> requestsById = new ConcurrentHashMap<>();
+	private final Map<Integer, Collection<Request>> requestsById = new ConcurrentHashMap<>();
 
 	@Autowired
 	private PlatformTransactionManager transactionManager;
 
-	private Collection<Request<T>> getRequestList(int id)
+	private Collection<Request> getRequestList(int id)
 	{
 		return requestsById.computeIfAbsent(id, i -> Collections.synchronizedCollection(new ArrayList<>()));
 	}
@@ -33,74 +33,79 @@ public class ListDeferredResultService<T>
 
 	public Object getDeferredResult(int id, Supplier<List<T>> listSupplier)
 	{
-		Request<T> request = new Request<>(listSupplier);
+		Request request = new Request(id, listSupplier);
+		addRequest(request);
+		return request.getDeferredResult();
+	}
 
+	private void addRequest(Request request)
+	{
 		CompletableFuture.runAsync(() ->
 		{
 			new TransactionTemplate(transactionManager).executeWithoutResult(status ->
 			{
 				if (!request.checkResult())
-					getRequestList(id).add(request);
+					getRequestList(request.id).add(request);
 			});
 		});
-
-		return request.getDeferredResult();
-	}
-}
-
-class Request<T>
-{
-	private static final long TIMEOUT = 30 * 1000;
-
-	private final Supplier<List<T>> listSupplier;
-	private final DeferredResult<List<T>> deferredResult;
-
-	public Request(Supplier<List<T>> listSupplier)
-	{
-		this.listSupplier = listSupplier;
-		this.deferredResult = new DeferredResult<>(TIMEOUT, new ResponseEntity<Void>(HttpStatus.REQUEST_TIMEOUT));
 	}
 
-	public DeferredResult<List<T>> getDeferredResult()
+	private class Request
 	{
-		return deferredResult;
-	}
+		private static final long TIMEOUT = 30 * 1000;
 
-	public boolean checkResult()
-	{
-		List<T> resultTmp = null;
-		Exception exceptionTmp = null;
+		private final int id;
+		private final Supplier<List<T>> listSupplier;
+		private final DeferredResult<List<T>> deferredResult;
 
-		try
+		private Request(int id, Supplier<List<T>> listSupplier)
 		{
-			resultTmp = listSupplier.get();
-			Objects.requireNonNull(resultTmp);
-		}
-		catch (Exception e)
-		{
-			exceptionTmp = e;
+			this.id = id;
+			this.listSupplier = listSupplier;
+			this.deferredResult = new DeferredResult<>(TIMEOUT, new ResponseEntity<Void>(HttpStatus.REQUEST_TIMEOUT));
 		}
 
-		Exception exception = exceptionTmp;
-		List<T> result = resultTmp;
-
-		if (result != null && result.isEmpty())
-			return false;
-
-		TransactionSynchronizationManager.registerSynchronization(new TransactionSynchronization()
+		private DeferredResult<List<T>> getDeferredResult()
 		{
-			@Override
-			public void afterCompletion(int status)
+			return deferredResult;
+		}
+
+		private boolean checkResult()
+		{
+			List<T> resultTmp = null;
+			Exception exceptionTmp = null;
+
+			try
 			{
-				if (status != TransactionSynchronization.STATUS_COMMITTED)
-					deferredResult.setErrorResult(new ResponseEntity<Void>(HttpStatus.REQUEST_TIMEOUT));
-				else if (exception != null)
-					deferredResult.setErrorResult(exception);
-				else
-					deferredResult.setResult(result);
+				resultTmp = listSupplier.get();
+				Objects.requireNonNull(resultTmp);
 			}
-		});
+			catch (Exception e)
+			{
+				exceptionTmp = e;
+			}
 
-		return true;
+			Exception exception = exceptionTmp;
+			List<T> result = resultTmp;
+
+			if (result != null && result.isEmpty())
+				return false;
+
+			TransactionSynchronizationManager.registerSynchronization(new TransactionSynchronization()
+			{
+				@Override
+				public void afterCompletion(int status)
+				{
+					if (status != TransactionSynchronization.STATUS_COMMITTED)
+						addRequest(Request.this);
+					else if (exception != null)
+						deferredResult.setErrorResult(exception);
+					else
+						deferredResult.setResult(result);
+				}
+			});
+
+			return true;
+		}
 	}
 }
